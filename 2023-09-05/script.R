105f5770b901de847eb76066fc4c41aa218050e0
Elise Maigné <elise.maigne@inrae.fr>
2023-09-06 15:01:31 +0200

    add contrib for week 36

---
+++ 2023-09-05/script.R
@@ -0,0 +1,15 @@
+####################################################
+##### Load the libraries
+
+library(data.table)
+library(ggplot2)
+library(tidytuesdayR)
+# library(ggridges)
+library(hrbrthemes)
+library(patchwork)
+
+###################################################
+##### Get the data
+
+tuesdata <- tidytuesdayR::tt_load('2023-09-05')
+
